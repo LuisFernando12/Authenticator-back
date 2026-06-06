aa90172d661c5b41611e80e19cc1f5a0c42f2c01
Luis Fernando <luisfernando201671@gmail.com>
2026-06-05 23:03:02 -0300

    refactor(app.module): remove unused entity imports from AppModule

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,16 +1,12 @@
 import { AuthModule } from '@/auth/infrastructure/module/auth.module';
 import { ClientModule } from '@/client/infrastructure/module/client.module';
 import { TokenModule } from '@/token/infrastructure/module/token.module';
-import { TokenEntity } from '@/token/infrastructure/persistence/entity/token.entity';
 import { UserModule } from '@/user/infrastructure/module/user.module';
-import { UserEntity } from '@/user/infrastructure/persistence/entity/user.entity';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClientEntity } from '../client/infrastructure/persistence/entity/client.entity';
-import { ConsentEntity } from '../consent/infrastructure/persistence/entity/consent.entity';
 import { AppConfigModule } from '../core/infrastructure/module/app-config.module';
 import { EmailModule } from '../core/infrastructure/module/email.module';
 import { HealthModule } from '../core/infrastructure/module/health.module';
@@ -29,7 +25,6 @@ import { OauthModule } from '../oauth/infrastructure/module/oauth.module';
           username: config.get('DB_USER'),
           password: config.get('DB_PASSWORD'),
           database: config.get('DB_NAME'),
-          entities: [UserEntity, TokenEntity, ClientEntity, ConsentEntity],
           synchronize: false,
           autoLoadEntities: true,
           migrationsRun: true,
