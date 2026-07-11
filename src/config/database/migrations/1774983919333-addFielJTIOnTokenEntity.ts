36871054d88ed91f2249a7c2da79d40b33b2904d
Luis Fernando <luisfernando201671@gmail.com>
2026-07-11 17:51:57 -0300

    refactor: optimize token migration by preventing duplicate column and index creation

--- src/config/database/migrations/1774983919333-addFielJTIOnTokenEntity.ts
+++ src/config/database/migrations/1774983919333-addFielJTIOnTokenEntity.ts
@@ -5,13 +5,18 @@ export class AddFieldJTIOnTokenEntity1774983919333 implements MigrationInterface
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     const hasColumn = await queryRunner.hasColumn('token', 'jti');
-    if (hasColumn) {
-      return;
+    if (!hasColumn) {
+      await queryRunner.query(`ALTER TABLE "token" ADD "jti" uuid NOT NULL`);
     }
-    await queryRunner.query(`ALTER TABLE "token" ADD "jti" uuid NOT NULL`);
-    await queryRunner.query(
-      `ALTER TABLE "token" ADD CONSTRAINT "UQ_7e0bb4f81da27f8a13b93d25b72" UNIQUE ("jti")`,
+    const table = await queryRunner.getTable('token');
+    const hasIndex = table.uniques.some(
+      (column) => column.name === 'UQ_7e0bb4f81da27f8a13b93d25b72',
     );
+    if (!hasIndex) {
+      await queryRunner.query(
+        `ALTER TABLE "token" ADD CONSTRAINT "UQ_7e0bb4f81da27f8a13b93d25b72" UNIQUE ("jti")`,
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
