ed8a6ed2b5dbc848afcddcb564e69ce6d90473d3
Luis Fernando <luisfernando201671@gmail.com>
2026-07-12 22:59:44 -0300

    refactor: simplify job processing logic in EmailWorker and enhance error handling

--- src/email/infrastructure/worker/email.worker.ts
+++ src/email/infrastructure/worker/email.worker.ts
@@ -3,7 +3,6 @@ import { Job } from 'bullmq';
 import { EmailLoggerPort } from '../../application/port/email-logger.port';
 import { EmailService } from '../../application/service/email.service';
 import { Email } from '../../domain/entity/email.entity';
-import { EmailDomainError } from '../../domain/error/email-domain.error';
 
 @Processor('send-email')
 export class EmailWorker extends WorkerHost {
@@ -15,36 +14,33 @@ export class EmailWorker extends WorkerHost {
     this.logger.log('EmailWorker created', { context: 'EmailWorker' });
   }
   async process(job: Job<Email>): Promise<void> {
-    this.logger.log('Processing job...', {});
-    if (job.name === 'reset-password') {
-      this.logger.log('Email to reset password', {});
-      await this.emailService.sendResetPasswordEmail(job.data);
-    }
-    if (job.name === 'activation') {
-      this.logger.log('Email to activation account', {});
-      await this.emailService.sendActivationAccountEmail(job.data);
-    }
-    job
-      .isFailed()
-      .then((isFailed) => {
-        if (isFailed) {
-          this.logger.log(`Failed processing job with jobId: ${job.id}`, {
-            errorStack: { cause: job.failedReason },
-          });
-          job.log(`Failed processing job with jobId: ${job.id}`);
-          return;
-        }
-        this.logger.log(`Completed processing job with jobId: ${job.id}`, {});
-        job.log('Completed processing job');
-        job.updateProgress(Number(job.progress) + 1);
-      })
-      .catch((error) => {
-        this.logger.error(`Failed processing job with jobId: ${job.id}`, {
-          errorStack: { cause: error },
-        });
-        throw EmailDomainError.internalServerError(
-          'Failure processing job on worker',
-        );
+    try {
+      this.logger.log('Processing job...', {});
+      switch (job.name) {
+        case 'activation':
+          this.logger.log('Email to activation account', {});
+          await this.emailService.sendActivationAccountEmail(job.data);
+          break;
+        case 'reset-password':
+          this.logger.log('Email to reset password', {});
+          await this.emailService.sendResetPasswordEmail(job.data);
+          break;
+        default:
+          this.logger.error('Email type not found', {});
+          throw new Error('Email type not found');
+      }
+
+      job.updateProgress(Number(job.progress) + 1);
+      this.logger.log(`Completed processing job with jobId: ${job.id}`, {});
+    } catch (error: any) {
+      this.logger.error(`Failed processing job with jobId: ${job.id}`, {
+        errorStack: {
+          message: error.message,
+          stack: error.stack,
+          cause: error.cause,
+        },
       });
+      throw error;
+    }
   }
 }
